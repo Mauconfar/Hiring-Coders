8ee9e4e26608851fe17b0f6065247c386dc6c886
Maucon Rodrigues <mauconfar@gmail.com>
2022-05-20 16:00:54 -0300

    Iniciando Typescript

---
+++ Fase 2/04 - Typescript/Aula 01 - introducao/src/exemplo-2.js
@@ -0,0 +1,6 @@
+function soma(a, b) {
+    return a + b;
+};
+
+soma(1, 1); //Ocorre a soma.
+soma("1", "1"); //ocorre a concatenação.
---
+++ Fase 2/04 - Typescript/Aula 01 - introducao/src/exemplo.ts
@@ -0,0 +1,6 @@
+function soma(a: number, b: number) { //Tipando as variáveis.
+    return a + b;
+};
+
+soma(1, 1); 
+soma("1", "1");
