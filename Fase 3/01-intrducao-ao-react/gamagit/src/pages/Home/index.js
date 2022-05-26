929a645bd49be6c598d458349af85077bf03f7e7
Maucon Rodrigues <mauconfar@gmail.com>
2022-05-26 15:15:31 -0300

    Finalizando aulas de React

--- Fase 3/01-intrducao-ao-react/gamagit/src/pages/Home/index.js
+++ Fase 3/01-intrducao-ao-react/gamagit/src/pages/Home/index.js
@@ -1,27 +1,41 @@
 import { useState } from 'react';
 import axios from 'axios';
 import * as S from './styled';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function App() {
-  const [ usuario, setUsuario] = useState('');
+  const navigate = useNavigate();
+  const [usuario, setUsuario] = useState('');
+  const [ erro, setErro] = useState(false);
 
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then( response =>  {
+
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
       const repositories = response.data;
+
       const repositoriesName = [];
-      repositories.map((repository) => {
-        repositoriesName.push(repositoriesName);
-      });
-      console.log(JSON.stringify(repositoriesName));
-    });
-  }
+      repositories.forEach((item) => repositoriesName.push(item.name));
+
+      localStorage.setItem('RepositoriesName', JSON.stringify(repositoriesName))
+      setErro(false)
+      navigate('/repositories')
+    })
+    .catch(err =>
+      setErro(true)
+      )
+
+  };
+
   return (
     <S.HomeContainer>
       <S.Content>
-        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} 
+                      onChange={e => { setUsuario(e.target.value); setErro(false)}} />
         <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
       </S.Content>
+
+      {erro ? <S.ErrorMsg> Ocorreu um erro. Tente Novamente! </S.ErrorMsg> : ''}
     </S.HomeContainer>
   );
 }
--- Fase 3/01-intrducao-ao-react/gamagit/src/pages/Repositories/index.js
+++ Fase 3/01-intrducao-ao-react/gamagit/src/pages/Repositories/index.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from './styled'; //Importar tudo através da variável "S"
+import { useNavigate } from "react-router-dom";
 
-export default function Repositories(){
 
-    return(
+export default function Repositories() {
+    const navigate = useNavigate();
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+
+        let repositoriesName = localStorage.getItem('RepositoriesName');
+
+        console.log(repositoriesName)
+
+        if (repositoriesName) {
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            navigate("/")
+        }
+
+    }, [navigate]);
+
+    return (
         <S.Container>
             <S.Title> Repositorios</S.Title>
             <S.List>
-                <S.ListItem> Repositorio: Nome do Repositorio</S.ListItem>
+                {repositories.map((repository, index) => <S.ListItem key={index}> Repositório: {repository} </S.ListItem>)}
             </S.List>
+            <S.LinkHome to="/"> Voltar </S.LinkHome>
         </S.Container>
     )
 }
